b367d9cf60d0a87288b82eec4b84b6801a3ad40b
Marcelo Viana <marcelorsviana@gmail.com>
2022-01-31 12:20:08 -0300

    Initial commit - the main() function and 'kotlin.Unit'

---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println("Hello, ${args[0]}!")
+
+    println()
+    val isUnit = println("This is an expression")
+    println(isUnit)
+
+    println()
+    val temperature = 10
+    val isHot = if (temperature > 50) true else false
+    println(isHot)
+
+    println()
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
+
+}
