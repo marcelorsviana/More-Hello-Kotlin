533e42c0a5dd95b3c543b86f7923336b3da2c780
Marcelo Viana <marcelorsviana@gmail.com>
2022-01-31 21:51:35 -0300

    Show more about Kotlin functions

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,17 +1,55 @@
-fun main(args: Array<String>) {
-    println("Hello, ${args[0]}!")
+import java.util.*
 
-    println()
-    val isUnit = println("This is an expression")
-    println(isUnit)
+fun randomDay(): String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
 
-    println()
-    val temperature = 10
-    val isHot = if (temperature > 50) true else false
-    println(isHot)
+/*
+assinging the result of a 'when' loop to a val
+ */
 
-    println()
-    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
-    println(message)
+//fun fishFood(day: String) : String {
+//    val food = ""
+//    when (day){
+//        "Monday" -> food = "flakes"
+//        //"Tuesday" -> food = "pellets"
+//        "Wednesday" -> food = "redworms"
+//        "Thursday" -> food = "granules"
+//        "Friday" -> food = "mosquitoes"
+//        //"Saturday" -> food = "lettuce"
+//        "Sunday" -> food = "plankton"
+//        else -> food = "nothing"
+//    }
+//    return food
+//}
+
+
+/*
+The same loop, but more concise.
+ */
+
+fun fishFood(day: String) : String {
+    return when (day){
+        "Monday" -> "flakes"
+        //"Tuesday" -> "pellets"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        //"Saturday" -> "lettuce"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+}
+
+fun main() {
+
+    feedTheFish()
 
 }
