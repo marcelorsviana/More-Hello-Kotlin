f89f321df4fcf6a18854ae4c2033a72b585bec8f
Marcelo Viana <marcelorsviana@gmail.com>
2022-01-31 22:30:55 -0300

    Add compact functions

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -46,10 +46,48 @@ fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
     println("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
 }
+
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
+}
+
+//fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20) : Boolean {
+//    return when {
+//        temperature > 30 -> true
+//        dirty > 30       -> true
+//        day == "Sunday"  -> true
+//        else             -> false
+//    }
+//}
+
+/*
+The function below is a version the above one, but with compact functions inside it, instead.
+ */
+
+fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20) : Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty)        -> true
+        isSunday(day)         -> true
+        else                  -> false
+    }
+}
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
+
 
 fun main() {
 
     feedTheFish()
+    swim()                      // default speed
+    swim("slow")          // positional argument
+    swim(speed="turtle-like")   // named parameter
+
 
 }
